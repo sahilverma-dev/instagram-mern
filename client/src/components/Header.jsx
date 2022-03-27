ada7a505341ebb1debab6ea79711f0523b322438
theviralboy <domainsahil007@gmail.com>
2022-03-27 09:10:00 +0530

    nav menu responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -62,53 +62,66 @@ const Header = () => {
     return handleScroll();
   }, []);
   return (
-    <div
-      className={`px-4 ${
-        sticky
-          ? "sticky top-0 py-4 shadow-lg z-50 bg-white md:bg-opacity-70 md:backdrop-blur"
-          : "md:py-4 py-2 bg-white"
-      }  w-full transition-all`}
-    >
-      <div className="max-w-7xl mx-auto flex  items-center justify-between">
-        <Link to="/">
-          <div className="flex items-center gap-1">
-            <InstagramIcon color="#343434" size={20} />
-            <img
-              src="./images/logo.png"
-              alt="logo"
-              className="h-8 w-auto object-fill"
-            />
-          </div>
-        </Link>
-        <div className="md:flex hidden items-center gap-6">
-          {navigationData.map((item, index) => (
-            <NavLink
-              key={index}
-              to={item.path}
-              className="relative group aspect-square border border-transparent transition-all hover:border-black hover:scale-110 p-2 rounded-md"
-              activeIcon={item.activeIcon}
-              inActiveIcon={item.inActiveIcon}
-            />
-          ))}
-        </div>
-        {user && (
-          <div className="flex items-center justify-end w-[120px] gap-3">
-            <Link to="/chat" className="block relative p-2 aspect-square">
-              {/* <div className="absolute top-0 right-0 aspect-square p-1 rounded-full text-white font-semibold bg-red-600"></div> */}
-              <SendIcon size={20} />
-            </Link>
-            <Link to="/profile">
+    <>
+      <div
+        className={`px-4 ${
+          sticky
+            ? "sticky top-0 py-4 shadow-lg z-50 bg-white md:bg-opacity-70 md:backdrop-blur"
+            : "md:py-4 py-2 bg-white"
+        }  w-full transition-all`}
+      >
+        <div className="max-w-7xl mx-auto flex  items-center justify-between">
+          <Link to="/">
+            <div className="flex items-center gap-1">
+              <InstagramIcon color="#343434" size={20} />
               <img
-                src={user.profilePic}
-                className="block h-7 w-7 aspect-square rounded-full border border-black"
-                alt={user.name}
-                // onClick={logout}
+                src="./images/logo.png"
+                alt="logo"
+                className="h-8 w-auto object-fill"
               />
-            </Link>
+            </div>
+          </Link>
+          <div className="md:flex hidden items-center gap-6">
+            {navigationData.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.path}
+                className="relative group aspect-square border border-transparent transition-all hover:border-black hover:scale-110 p-2 rounded-md"
+                activeIcon={item.activeIcon}
+                inActiveIcon={item.inActiveIcon}
+              />
+            ))}
           </div>
-        )}
+          {user && (
+            <div className="flex items-center justify-end w-[120px] gap-3">
+              <Link to="/chat" className="block relative p-2 aspect-square">
+                {/* <div className="absolute top-0 right-0 aspect-square p-1 rounded-full text-white font-semibold bg-red-600"></div> */}
+                <SendIcon size={20} />
+              </Link>
+              <Link to="/profile">
+                <img
+                  src={user.profilePic}
+                  className="block h-7 w-7 aspect-square rounded-full border border-black"
+                  alt={user.name}
+                  // onClick={logout}
+                />
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className="fixed bottom-0 left-0 w-full z-50 bg-white flex md:hidden items-center p-4 justify-around gap-6">
+        {navigationData.map((item, index) => (
+          <NavLink
+            key={index}
+            to={item.path}
+            className="relative group aspect-square border border-transparent transition-all hover:border-black hover:scale-110 p-2 rounded-md"
+            activeIcon={item.activeIcon}
+            inActiveIcon={item.inActiveIcon}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
